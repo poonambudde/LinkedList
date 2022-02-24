6714d2388265c7d9feee475ae40d86bb998c0f78
poonambudde <buddepoonam123@gmail.com>
2022-02-24 23:12:37 +0530

    [Poonam] ADDED: Welcome to Linked List Programs

---
+++ Linked List/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Linked_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To the Data structure program using Linked List");
+        }
+    }
+}
