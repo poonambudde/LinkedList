0f0863da71adef6cafcd14413eb2c4a7309eacdf
poonambudde <97157799+poonambudde@users.noreply.github.com>
2022-02-25 17:52:45 +0530

    [Poonam] ADDED: Ability to create Linked List by appending

--- Linked List/LinkListOperation.cs
+++ Linked List/LinkListOperation.cs
@@ -6,8 +6,7 @@ namespace Linked_List
 {
     class LinkListOperation
     {
-        public Node head;
-        
+        public Node head;  
         public void insert(int value)
         {
             Node newNode = new Node();
@@ -27,7 +26,6 @@ namespace Linked_List
                 head = newNode;
             }
         }
-
         public void Add(int value)
         {
             Node newNode = new Node();
@@ -37,16 +35,23 @@ namespace Linked_List
             head = newNode;
             newNode.next = temp;
         }
-
         public void display()
         {
+            if (head == null)
+            {
+                Console.WriteLine("Linked List is empty");
+            }
+            else
+            {
                 Node temp = head;
+
                 while (temp.next != null)
                 {
                     Console.WriteLine(temp.data);
                     temp = temp.next;
                 }
-                Console.WriteLine(temp.data);           
+                Console.WriteLine(temp.data);    
+            }           
         }
     }
 }
