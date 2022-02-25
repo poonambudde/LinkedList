e0762d8ba00202caf23300a2bf33233bd7962a21
poonambudde <97157799+poonambudde@users.noreply.github.com>
2022-02-25 16:12:01 +0530

    [Poonam] ADDED: create a simple Linked List

--- Linked List/LinkListOperation.cs
+++ Linked List/LinkListOperation.cs
@@ -7,17 +7,15 @@ namespace Linked_List
     class LinkListOperation
     {
         public Node head;
+        
         public void insert(int value)
         {
-
             Node newNode = new Node();
             newNode.data = value;
             newNode.next = null;
-
             if (head != null)
             {
                 Node temp = head;
-
                 while (temp.next != null)
                 {
                     temp = temp.next;
@@ -29,24 +27,16 @@ namespace Linked_List
                 head = newNode;
             }
         }
+        
         public void display()
         {
-            if (head == null)
-            {
-                Console.WriteLine("Linked List is empty");
-            }
-            else
-            {
                 Node temp = head;
-
                 while (temp.next != null)
                 {
                     Console.WriteLine(temp.data);
                     temp = temp.next;
-
                 }
-                Console.WriteLine(temp.data);
-            }
+                Console.WriteLine(temp.data);           
         }
     }
 }
